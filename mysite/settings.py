4ec7ab7ddc45e8d45e9ccdea450bf52fb8146e45
jacquelin_ledezma <jacquelin.ledezma7089@gmail.com>
2017-07-05 17:03:39 -0400

    My Django Girls second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b@z-99r(t5td+%ak1taik=7fp0bq#vvyuq)l$(&ck7u30j-a!$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','jacquelin7070.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
